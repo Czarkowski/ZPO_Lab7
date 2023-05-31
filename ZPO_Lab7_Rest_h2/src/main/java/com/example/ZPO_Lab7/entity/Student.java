3acd8b4b9776c0a6e758606d2c3f7863624bd4e3
Czarkowski <cezarytytko@gmail.com>
2023-05-31 15:08:28 +0200

    add student validation

--- ZPO_Lab7_Rest_h2/src/main/java/com/example/ZPO_Lab7/entity/Student.java
+++ ZPO_Lab7_Rest_h2/src/main/java/com/example/ZPO_Lab7/entity/Student.java
@@ -1,6 +1,10 @@
 package com.example.ZPO_Lab7.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,13 +21,17 @@ public class Student {
     private Long id;
 
     @Column(name = "first_name")
+    @NotBlank
+    @Pattern(regexp = "[A-Z][a-z]+")
     private String firstName;
 
     @Column(name = "last_name")
+    @NotBlank
+    @Pattern(regexp = "[A-Z][a-z]+")
     private String lastName;
 
     @Column(name = "age")
+    @Max(99)
+    @Min(10)
     private int age;
-
-    // Gettery, settery i inne pola
 }
